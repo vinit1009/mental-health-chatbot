0713a2af9fe1a15209a287e148b609580c86d758
Vinit <vinitshah258@gmail.com>
2023-12-25 15:21:36 -0500

    Setup index.html with bootstrap and botpress script

---
+++ src/components/Chatbot.js
@@ -0,0 +1,12 @@
+// src/components/Chatbot.js
+import React from 'react';
+
+function Chatbot() {
+  return (
+    <div className="chatbot-container" style={{ height: '500px', width: '100%' }}>
+      {/* Chatbot will be injected here */}
+    </div>
+  );
+}
+
+export default Chatbot;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 // src/pages/Home.js
 import React from 'react';
+import Chatbot from '../components/Chatbot';
 
 function Home() {
   return (
@@ -7,6 +8,13 @@ function Home() {
       <h1>Welcome to the Mental Health Chatbot</h1>
       <p>This is the home page where users can learn about the chatbot and how it can assist them.</p>
       {/* Additional content or components */}
+    </div>
+  );
+  return (
+    <div className="container">
+      <h1>Welcome to the Mental Health Chatbot</h1>
+      <p>This is the home page.</p>
+      <Chatbot />
     </div>
   );
 }
